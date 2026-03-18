f88b2b90ab70ebdee06abba079f781bf11d93c7a
Alex Oliveira <seuemail@exemplo.com>
2026-03-18 17:45:32 -0300

    cREATING pLAYERS E eNEMIES Moving Players e Enemies

---
+++ Enemy.py
@@ -0,0 +1,11 @@
+from Const import ENTITY_SPEED, WIN_WIDTH
+from Entity import Entity
+
+class Enemy(Entity):
+    def __init__(self, nome: str, position: tuple):
+      super().__init__(nome, position)
+
+    def move(self, ):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
