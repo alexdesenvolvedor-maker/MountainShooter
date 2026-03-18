732683e27948f06ce7f26952dfe2901ed11006e1
Alex Oliveira <seuemail@exemplo.com>
2026-03-18 10:53:07 -0300

    Creating menu interaction Creating ABC Class Creating Factor Creating Level 1 Bg Implemenation : paralaxx

---
+++ Background.py
@@ -0,0 +1,14 @@
+from abc import ABC
+
+from Const import WIN_WIDTH, ENTITY_SPEED
+from Entity import Entity
+
+
+class Background(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+
+    def move(self, ):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
